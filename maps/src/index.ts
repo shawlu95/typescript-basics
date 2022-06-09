fcca97e7cdd9c8dbb90b60b55bcd9ad9be549605
Shaw Lu <shawlu95@gmail.com>
2022-06-09 00:14:03 -0700

    Refactor CustomMap to expose minimum functionalities

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,6 +1,7 @@
 /// <reference types="@types/google.maps" />
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
 console.log(user);
@@ -8,10 +9,4 @@ console.log(user);
 const corp = new Company();
 console.log(corp);
 
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const customMap = new CustomMap('map');
