6ac6a8ca9bf60cb1b925e588523dc5d1b59727ec
Shaw Lu <shawlu95@gmail.com>
2022-06-09 00:24:39 -0700

    One solution to simplify code (accept multiple types)

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -7,5 +7,5 @@ const user = new User();
 const corp = new Company();
 const customMap = new CustomMap('map');
 
-customMap.addUserMarker(user);
-customMap.addCompanyMarker(corp);
+customMap.addMarker(user);
+customMap.addMarker(corp);
