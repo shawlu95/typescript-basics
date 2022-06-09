7c34002dfb455345060aedf86ea60bbc6f785033
Shaw Lu <shawlu95@gmail.com>
2022-06-09 00:35:17 -0700

    Add info window to map pins

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -20,12 +20,19 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'hi bailey',
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
